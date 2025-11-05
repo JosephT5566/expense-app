25114923ff6bfc0a4af839b69d6980411d203a97
JosephTseng <zxp930110@hotmail.com.tw>
2025-11-06 01:05:22 +0800

    implement search page for now

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
+
+<section class="card p-4">
+	<Icon icon="solar:confounded-circle-bold-duotone" width="24" height="24" />
+</section>
